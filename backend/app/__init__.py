ec08aa224f16ab450f822c49af5cda7b08ce1770
littleeov <eov.grafd@yandex.ru>
2025-05-15 21:51:48 +0500

    db?

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from backend.app.db.database import db
 
 def create_app():
     app = Flask(__name__)
@@ -7,4 +8,8 @@ def create_app():
     from .api.stress import stress_bp
     app.register_blueprint(stress_bp, url_prefix='/api')
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:littleeov@localhost:5432/stress_assessment_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
     return app
