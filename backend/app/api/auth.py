6272085c9b9e4000b249a9066c6403c452df8364
littleeov <eov.grafd@yandex.ru>
2025-06-05 14:51:15 +0500

    run_upgrade

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_user, logout_user, login_required
-from ..db.models import User  # Относительный импорт
+from ..db.models import User
 from ..db.extensions import db
 
 auth_bp = Blueprint('auth', __name__)
