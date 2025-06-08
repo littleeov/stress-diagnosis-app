766e808130c98c78788266c54dba905c47f7d7f6
littleeov <eov.grafd@yandex.ru>
2025-06-08 11:01:19 +0500

    refactoring_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { UserProvider, useUser } from './context/UserContext';
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import theme from './theme';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Analysis from './pages/Analysis';
-
-const theme = createTheme();
-
-const ProtectedRoute = ({ children }) => {
-  const { user } = useUser();
-  return user ? children : <Navigate to="/login" />;
-};
+// импортируйте другие страницы по мере необходимости
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <UserProvider>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/analysis"
-              element={
-                <ProtectedRoute>
-                  <Analysis />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </UserProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+          <Route path="/analysis" element={<Analysis />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
 
 export default App;
+
